fa4399a8dc6989dbabcf8a3f779a435960906e6c
Bence Töröcskei <torocskeibence@gmail.com>
2022-08-23 12:14:24 +0200

    Add: SquareTypes, Square - getter, setter

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -2,6 +2,17 @@
 
 public class Square {
 
+    private SquareType square;
 
+    private Square(SquareType square) {
+        this.square = square;
+    }
 
+    public SquareType getSquare() {
+        return square;
+    }
+
+    public void setSquare(SquareType square) {
+        this.square = square;
+    }
 }
