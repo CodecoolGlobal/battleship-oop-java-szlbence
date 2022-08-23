c7a5f15bca0572da55b331ceb6c344fae33f6809
Bence Szlavik <szlbence@gmail.com>
2022-08-23 15:42:24 +0200

    Implement isPlacementOk function

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -5,7 +5,7 @@ public class Board {
 
     final private Square[][] ocean;
 
-    Board(){
+    Board() {
 
         ocean = new Square[10][10];
         for (int row = 0; row < 10; row++) {
@@ -15,8 +15,61 @@ public class Board {
         }
     }
 
-    boolean isPlacementOk(){
-        return false;
-    }
+    boolean isPlacementOk(int Length, String direction, int[] startCoordinate) {
+        int userX = startCoordinate[0];
+        int userY = startCoordinate[1];
 
+        switch (direction) {
+            case "up":
+                if (userX - (Length - 1) < 0) {
+                    return false;
+                } else {
+                    for (int x = userX; x > x - Length; x--) {
+                        if (ocean[x][userY].getSquare() != SquareType.EMPTY) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+
+            case "down":
+                if (userX + (Length - 1) > 9) {
+                    return false;
+                }
+                else {
+                    for (int x = userX; x < x + Length; x++) {
+                        if (ocean[x][userY].getSquare() != SquareType.EMPTY) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            case "left":
+                if (userY - (Length - 1) < 0) {
+                    return false;
+                }
+                else {
+                    for (int y = userY; y > y - Length; y--) {
+                        if (ocean[userX][y].getSquare() != SquareType.EMPTY) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            case "right":
+                if (userY + (Length - 1) > 9) {
+                    return false;
+                }
+                else {
+                    for (int y = userY; y < y + Length; y++) {
+                        if (ocean[userX][y].getSquare() != SquareType.EMPTY) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+        }
+        return false;
+
+    }
 }
