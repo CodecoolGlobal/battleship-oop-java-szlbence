d52d6f05f4a653bb086791e44854b5cc258e6858
Bence Szlavik <szlbence@gmail.com>
2022-08-23 14:52:53 +0200

    Create board with empty squares

--- src/main/java/Battleship.java
+++ src/main/java/Battleship.java
@@ -1,4 +1,9 @@
+import static java.util.Arrays.deepToString;
+
 public class Battleship {
 
+    public static void main(String[] args) {
+
+    }
 
 }
--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,5 +1,19 @@
 public class Board {
-    Square [][] ocean;
+    public Square[][] getOcean() {
+        return ocean;
+    }
+
+    final private Square[][] ocean;
+
+    Board(){
+
+        ocean = new Square[10][10];
+        for (int row = 0; row < 10; row++) {
+            for (int col = 0; col < 10; col++) {
+                ocean[row][col] = new Square(SquareType.EMPTY);
+            }
+        }
+    }
 
     boolean isPlacementOk(){
         return false;
