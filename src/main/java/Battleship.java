d99f6fc78f3dffd68f2732ce56bc90383e7ded0d
Adrián Bükfa <bukfa.adrian@gmail.com>
2022-08-24 13:35:42 +0200

    Implement displayMenu

--- src/main/java/Battleship.java
+++ src/main/java/Battleship.java
@@ -1,11 +1,12 @@
-import static java.util.Arrays.deepToString;
 
 public class Battleship {
 
     public static void main(String[] args) {
        /* Board ocean = new Board();
         Square[][] testOcean = ocean.getOcean();
-        Display.displayOcean(testOcean);*/
+        Display.displayOcean(testOcean);
+        Display display = new Display();
+        display.displayMenu();**/
     }
 
 }
