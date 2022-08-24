6d71368707a9dce20e6c92be11a255b7cedf3b29
Bence Töröcskei <torocskeibence@gmail.com>
2022-08-24 11:36:28 +0200

    Add: manualPlacement full version

--- src/main/java/BoardFactory.java
+++ src/main/java/BoardFactory.java
@@ -8,31 +8,43 @@ public class BoardFactory {
     Square [][] ocean = board.getOcean();
 
 
-     public void manualPlacement(String direction, int [] startCoordinate, int Length){
+
+     public void manualPlacement(String direction, int [] startCoordinate, int Length, Ship ship){
          int userX = startCoordinate[0];
          int userY = startCoordinate[1];
+
 
          switch (direction) {
              case "up":
 
                      for (int x = userX; x > x - Length; x--) {
-                         ocean[x][userY].setSquare(SquareType.SHIP);}
+                         Square shipCoordinate =  ocean[x][userY];
+                         shipCoordinate.setSquare(SquareType.SHIP);
+                         ship.addShipCoordinates(shipCoordinate);
+                     }
 
              case "down":
 
-
-                     for (int x = userX; x < x + Length; x++) {
-                         ocean[x][userY].setSquare(SquareType.SHIP); }
+                    for (int x = userX; x < x + Length; x++) {
+                         Square shipCoordinate =  ocean[x][userY];
+                         shipCoordinate.setSquare(SquareType.SHIP);
+                         ship.addShipCoordinates(shipCoordinate);}
              case "left":
 
                      for (int y = userY; y > y - Length; y--) {
-                         ocean[userX][y].setSquare(SquareType.SHIP); }
+                         Square shipCoordinate = ocean[userX][y];
+                         shipCoordinate.setSquare(SquareType.SHIP);
+                         ship.addShipCoordinates(shipCoordinate);
+
+                     }
 
              case "right":
 
 
                      for (int y = userY; y < y + Length; y++) {
-                         ocean[userX][y].setSquare(SquareType.SHIP);
+                         Square shipCoordinate = ocean[userX][y];
+                         shipCoordinate.setSquare(SquareType.SHIP);
+                         ship.addShipCoordinates(shipCoordinate);
 
                          }
                      }
