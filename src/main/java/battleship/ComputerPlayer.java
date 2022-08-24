68919387e8bb51baf4d733399859eb0234b3038c
domi <szabo.domonkos97@gmail.com>
2022-08-24 22:20:51 +0200

    implement easy Ai

--- src/main/java/battleship/ComputerPlayer.java
+++ src/main/java/battleship/ComputerPlayer.java
@@ -1,4 +1,25 @@
 package battleship;
 
-public class ComputerPlayer {
+public class ComputerPlayer extends Player{
+
+    // takes random shots excluding already struck fields
+
+    // check what needs overriding
+    //      - shootEnemy
+    //      -
+
+    public void shootEnemy(Square[][] ocean, Ship ship){
+        int[] robotCoordinates = Util.generateRandomCoordinates();
+        int robotX = robotCoordinates[0];
+        int robotY = robotCoordinates[1];
+        if (isHit(robotCoordinates, ship)){
+            ocean[robotX][robotY].setSquare(SquareType.HIT);
+        } else {
+            ocean[robotX][robotY].setSquare(SquareType.MISSED);
+        }
+
+    }
+
+
+
 }
